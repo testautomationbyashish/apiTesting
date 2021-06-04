a6a99df8d61ba670c41f61b57343c9a333e65f08
testautomationbyashish <85038148+testautomationbyashish@users.noreply.github.com>
2021-06-04 10:10:51 +0530

    Update testCreateNewRocket.java

--- src/test/java/stepdefs/testCreateNewRocket.java
+++ src/test/java/stepdefs/testCreateNewRocket.java
@@ -30,10 +30,10 @@ public class test04june2021 {
        .body("second_stage[0].thrust.kN",equalTo(31))
        .body("diameter[0].feet",equalTo(5.5F))
        .body("landing_legs[1].material",equalTo("carbon fiber"))
-       .body("payload_weights[0].name",equalTo("Low Earth Orbit"))
-       .body("success_rate_pct[2]",equalTo("98"))
-       .body("mass[2]",equalTo("1420788"))
-       .body("propellant_1[3]",equalTo("liquid oxygen"));
+       .body("cost_per_launch[3]",equalTo(7000000))
+       .body("success_rate_pct[2]",equalTo(100))
+       .body("mass[2].kg",equalTo(1420788))
+       .body("engines[0].propellant_1",equalTo("liquid oxygen"));
        
  
 
@@ -42,7 +42,8 @@ public class test04june2021 {
 	@Test(priority = 2)
 	public void test_addnewRocket()
 	{
-		HashMap data = new HashMap<>();
+		HashMap data = new HashMap();
+		data.put("id", "33");
 		data.put("name", "rocketsingh1");
 		data.put("type", "rocket");
 		data.put("active", "false");
@@ -66,5 +67,54 @@ public class test04june2021 {
 		
 		
 	}
+
+	@Test(priority = 3)
+	public void test_updateExistingRocket()
+	{
+		HashMap data = new HashMap();
+		data.put("id", "5e9d0d95eda69955f709d1eb");
+		data.put("name", "rocketsingh2");
+		data.put("type", "rocket");
+		data.put("active", "false");
+		data.put("boosters", "1");
+		
+		
+		given()
+		.contentType("application/json")
+		.headers("x-api-Key", "spacex-key")
+		.body(data)
+		.when()
+		        .patch("https://api.spacexdata.com/v4/rockets/5e9d0d95eda69955f709d1eb")
+		.then()
+		        .statusCode(200)
+		        .log().body()
+		        .body("name[5]",equalTo("rocketsingh2"))
+		        .body("type[5]",equalTo("rocket"));
+		        
+		        	}
+
 	
+	@Test(priority = 4)
+	public void test_deleteExistingRocket()
+	{
+		
+		
+		Response res=
+		given()
+		.contentType("application/json")
+		.headers("x-api-Key", "spacex-key")
+		
+		.when()
+		        .delete("https://api.spacexdata.com/v4/rockets/5e9d0d95eda69955f709d1eb")
+		.then()
+		        .statusCode(200)
+		        .log().body()
+		        .extract().response();
+		        
+		 String jsonString = res.asString()  ;
+		Assert.assertEquals(jsonString.contains("Record deleted successfully"), true);
+		        
+		        	}
+
+
 }
